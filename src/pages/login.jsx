c7eed5d9f74ef36e043f738f0ba7aafc2ec2029e
TopazKang <lasksla@gmail.com>
2024-05-16 00:15:19 +0900

    feature/login 0

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,8 +2,20 @@ import styled from "styled-components";
 
 function Login() {
   return (
-    <p>로그인</p>
+    <Container>
+      <p>로그인</p>
+    </Container>
+
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #F4F5F7;
+  min-width: 100vh;
+  min-height: 100vh;
+`
 
 export default Login;
